a2390457a5331b82c50812a72c13d858153a9a9f
chris-bacon <chrisbacon2009@hotmail.co.uk>
2018-03-08 17:38:43 +0000

    Refactored out output buffer

--- Backend/CodeGenerator.hs
+++ Backend/CodeGenerator.hs
@@ -3,34 +3,34 @@ module CodeGenerator where
 generateLaTeX :: [(String, String)] -> String
 generateLaTeX intRep = go intRep "" 
     where
-        go :: [(String, String)] -> String
+        go :: [(String, String)] -> String -> String
         go [] output = output
         go intRep output
             -- Opening tags
-            | token == "sTag" && attribute == "startDocument" = generateLaTeX (tail intRep) (output ++ "\\documentclass[11pt]{article}\n\n\\begin{document}\n\n")
-            | token == "sTag" && attribute == "bold" = generateLaTeX (tail intRep) (output ++ "\\textbf{")
-            | token == "sTag" && attribute == "italics" = generateLaTeX (tail intRep) (output ++ "\\textit{")
-            | token == "sTag" && attribute == "em" = generateLaTeX (tail intRep) (output ++ "\\textit{")
-            | token == "sTag" && attribute == "pre" = generateLaTeX (tail intRep) (output ++ "\\begin{verbatim}")
+            | token == "sTag" && attribute == "startDocument" = go (tail intRep) (output ++ "\\documentclass[11pt]{article}\n\n\\begin{document}\n\n")
+            | token == "sTag" && attribute == "bold" = go (tail intRep) (output ++ "\\textbf{")
+            | token == "sTag" && attribute == "italics" = go (tail intRep) (output ++ "\\textit{")
+            | token == "sTag" && attribute == "em" = go (tail intRep) (output ++ "\\textit{")
+            | token == "sTag" && attribute == "pre" = go (tail intRep) (output ++ "\\begin{verbatim}")
 
             -- Closing tags
-            | token == "eTag" && attribute == "p" = generateLaTeX (tail intRep) (output ++ "\n\n")
-            | token == "eTag" && attribute == "pre" = generateLaTeX (tail intRep) (output ++ "\\end{verbatim}")
+            | token == "eTag" && attribute == "p" = go (tail intRep) (output ++ "\n\n")
+            | token == "eTag" && attribute == "pre" = go (tail intRep) (output ++ "\\end{verbatim}")
             | token == "eTag" &&
                 attribute == "bold" ||
                 attribute == "italics" ||
-                attribute == "em" = generateLaTeX (tail intRep) (output ++ "}")
-            | token == "eTag" && attribute == "endDocument" = generateLaTeX (tail intRep) (output ++ "\\end{document}")
+                attribute == "em" = go (tail intRep) (output ++ "}")
+            | token == "eTag" && attribute == "endDocument" = go (tail intRep) (output ++ "\\end{document}")
 
             -- non-tags
             -- if two words in a sequence, we want whitespace
-            | token == "Word" && lookahead == "Word" = generateLaTeX (tail intRep) (output ++ attribute ++ " ")
-            | token == "Word" = generateLaTeX (tail intRep) (output ++ attribute)
-            | token == "Lit" = generateLaTeX (tail intRep) (output ++ attribute)
-            | token == "Op" = generateLaTeX (tail intRep) (output ++ " " ++ attribute)
+            | token == "Word" && lookahead == "Word" = go (tail intRep) (output ++ attribute ++ " ")
+            | token == "Word" = go (tail intRep) (output ++ attribute)
+            | token == "Lit" = go (tail intRep) (output ++ attribute)
+            | token == "Op" = go (tail intRep) (output ++ " " ++ attribute)
             -- We may not want to convert every token into a LaTeX representation
             -- And we are assuming that all errors have already been caught by now
-            | otherwise = generateLaTeX (tail intRep) output
+            | otherwise = go (tail intRep) output
             where
                 token = fst $ head intRep
                 lookahead = fst $ f (tail intRep)
