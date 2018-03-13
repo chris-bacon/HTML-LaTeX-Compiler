e31d4538664f287bb17f0339e4b6408f4fab887f
chris-bacon <chrisbacon2009@hotmail.com>
2018-03-13 22:09:37 +0000

    updated test for parser

--- tests/LexicalAnalyzerTest.hs
+++ tests/LexicalAnalyzerTest.hs
@@ -11,27 +11,17 @@ input1 = "<html></html>"
 input2 = "<html>hello</html>"
 input3 = "<html><html></html></html>"
 input4 = "<html><html><p>This is a red dog</p></html></html>"
+input5 = "<html><b>red</b></html>"
 
 testLexicalAnalyzer = hspec $ do
     describe "LexicalAnalyzer" $ do
         describe "run" $ do
+            it "returns \"HTMLTag (Text \"\")\" for input of \"<html></html>\"" $ do
+                let (Right result) = LA.run input1
+                result `shouldBe` LA.HTMLTag (LA.Text "")
             it "returns \"HTMLTag (HTMLTag (ParaTag (Text \"This is a red dog\")))\" for input of \"<html><html><p>This is a red dog</p></html></html>\"" $ do
                 let (Right result) = LA.run input4
                 result `shouldBe` LA.HTMLTag (LA.HTMLTag (LA.ParaTag (LA.Text "This is a red dog")))
-        -- describe "isLetterDigit" $ do
-        --     it "returns true when given a letter" $ do
-        --         LA.isLetterDigit "a" `shouldBe` True
-        --     it "returns true when given a digit" $ do
-        --         LA.isLetterDigit "1" `shouldBe` True
-        --     it "returns false when given punctuation" $ do
-        --         LA.isLetterDigit ";" `shouldBe` False
-        --     it "returns true when given an empty string" $ do
-        --         LA.isBufferClear "" `shouldBe` True
-        --     it "returns false when given a non-empty string" $ do
-        --         LA.isBufferClear "red" `shouldBe` False
-        -- describe "lexAnalyzer" $ do
-        --     it "should tokenize testHTML into lexemes" $ do
-        --         LA.lexAnalyzer (splitOn "" testHTML) `shouldBe` [("sTag", "p"), ("Word", "the"), ("Word", "red"), ("Word", "dog"), ("eTag", "p")]
-        --     it "should tokenize testHTML2 into lexemes" $ do
-        --         LA.lexAnalyzer (splitOn "" testHTML2) `shouldBe` [("sTag", "p"), ("sTag", "bold"), ("Word", "the"), ("Word", "red"), ("Word", "dog"), ("eTag", "bold"), ("eTag", "p")]
-
+            it "returns \"HTMLTag (BoldTag (Text \"red\")\" for input of \"<html><b>red</b></html>\"" $ do
+                let (Right result) = LA.run input5
+                result `shouldBe` LA.HTMLTag (LA.BoldTag (LA.Text "red"))
